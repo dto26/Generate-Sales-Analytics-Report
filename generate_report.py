423a73c5e9cd5c166e316c9fb817fbd4274ad7e5
dto26 <dto26@drexel.edu>
2021-02-08 17:10:46 -0500

    UPDATED generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -2,6 +2,7 @@
 from fpdf import FPDF
 from datetime import datetime, timedelta
 import os
+import analysis
 
 WIDTH = 210
 HEIGHT = 297
@@ -25,6 +26,18 @@ def create_sales_analytics_report(day=TEST_DATE, filename="report.pdf"):
     pdf.image("./resources/pdf-header.png", 0, 0, WIDTH)
     create_title(day, pdf)
 
+    # Generate plots
+    # plot_world_sales()
+    # plot_monthly_sales()
+    # plot_country_sales()
+    # plot_product_sales()
+    # plot_orders_vs_price()
+
+    # Add figures to pdf
+    # pdf.image('./resources/world_sales.png', 5, 20, WIDTH-20)
+    pdf.image('./resources/monthly_sales_plot.png', 5, 200, WIDTH/2-10)
+    pdf.image('./resources/orders_vs_price.png', WIDTH/2, 200, WIDTH/2-10)
+
     ''' Second Page '''
 
     ''' Third Page '''
@@ -32,7 +45,8 @@ def create_sales_analytics_report(day=TEST_DATE, filename="report.pdf"):
     pdf.output(filename, 'F')
 
 if __name__ == '__main__':
-    yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y").replace("/0","/").lstrip("0")
-    yesterday = "2020-12-25" # Uncomment line for testing
+    # yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y").replace("/0","/").lstrip("0")
+    yesterday = "2021-02-07" # Uncomment line for testing
 
     create_sales_analytics_report(yesterday)
+    print('Sales Analysis Report created successfully')
